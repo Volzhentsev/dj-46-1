17cace41449f7eed79b8d1d5134c33de2670922e
Volzhentsev Sergey <volzhentsev@bk.ru>
2022-03-14 20:10:35 +0300

    fist commit

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import datetime
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    path = os.getcwd()
+    dirct = os.listdir(path)
+    return HttpResponse(dirct)
+    raise NotImplemented
